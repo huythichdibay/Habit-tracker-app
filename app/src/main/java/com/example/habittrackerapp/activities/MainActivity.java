775b0d5d4aec80b8fffe604440251f953df80847
“Huydz” <huynguyen07052001@gmail.com>
2024-05-05 15:57:38 +0700

    add fragment

--- app/src/main/java/com/example/habittrackerapp/activities/MainActivity.java
+++ app/src/main/java/com/example/habittrackerapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.habittrackerapp;
+package com.example.habittrackerapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.habittrackerapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
