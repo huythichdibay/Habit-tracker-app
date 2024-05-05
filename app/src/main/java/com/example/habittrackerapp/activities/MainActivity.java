facb9b49fd7a5a1b2a9daf363f0601f4eea214a4
“Huydz” <huynguyen07052001@gmail.com>
2024-05-05 16:04:56 +0700

    update navigation between fragments

--- app/src/main/java/com/example/habittrackerapp/activities/MainActivity.java
+++ app/src/main/java/com/example/habittrackerapp/activities/MainActivity.java
@@ -1,16 +1,37 @@
 package com.example.habittrackerapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.habittrackerapp.R;
+import com.example.habittrackerapp.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //set up bottom navigation
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.dashboardFragment,
+                R.id.allHabitsFragment,
+                R.id.statsFragment,
+                R.id.settingsFragment)
+                .build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 }
